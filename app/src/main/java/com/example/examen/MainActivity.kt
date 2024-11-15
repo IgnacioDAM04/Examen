2766ef73cfdde82891164aaf1f3b08a1fa6519ca
tarde <IgnacioCedres.dam@gmail.com>
2024-11-15 15:51:00 +0000

    primer commit

---
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.examen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.examen.ui.theme.ExamenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExamenTheme {
+               Surface (modifier = Modifier
+                   .fillMaxSize()
+                   .background(background4)) {
+                   Container(MessageData.conversationSample)
+               }
+            }
+        }
+    }
+}
+
+//Numero de lista: 4
+
+val alumno4 = Color(0xFFA7FFEB)
+val background4 = Color(0xFF006064)
+
+
+data class Message(val author: String, val body: String)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Container(message: List<Message>) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = background4,
+                    titleContentColor = alumno4,
+
+                ),
+
+                title = {
+                        Text("Conversación Ignacio")
+                }
+            )
+        },
+
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            LazyColumn {
+                items(message) { message ->
+                    mensage(message)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun mensage(msg: Message) {
+
+
+
+
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profesor),
+            contentDescription = "Icono de perfil",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary)
+        )
+
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp, color = surfaceColor, modifier = Modifier
+                .animateContentSize()
+                .padding(1.dp)) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
+        }}
+
+}
+
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ExamenTheme {
+        Container(MessageData.conversationSample)
+    }
+}
+
+private val LightColorScheme = lightColorScheme(
+    background = Color.Red,
+    surface = Color.Red
+)
